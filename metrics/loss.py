836917552ba5c31b2d893e76bbab8cf2fb5b343c
ModDR6 <mkhal027@fiu.edu>
2023-07-29 23:04:40 +0100

    Added Metrics: True Positive, False Positive, False Negative, Precision, Recall, F1 Score

--- metrics/loss.py
+++ metrics/loss.py
@@ -17,4 +17,28 @@ def binary_cross_entropy(y_true, y_pred):
      m =  y_pred.shape[0]
      return -1/m * (np.dot(y_true, np.log(y_pred)) + np.dot((1-y_true), np.log(1-y_pred)))
 
+def true_positive(y_true, y_pred):
+    return np.sum((y_pred == 1) & (y_true == 1))
+
+def false_positive(y_true, y_pred):
+    return np.sum((y_pred == 1) & (y_true == 0))
+
+def false_negative(y_true, y_pred):
+    return np.sum((y_pred == 0) & (y_true == 1))
+
+def precision(y_true, y_pred):
+    tp = true_positive(y_true, y_pred)
+    fp = false_positive(y_true, y_pred)
+    return tp / (tp + fp)
+
+def recall(y_true, y_pred):
+    tp = true_positive(y_true, y_pred)
+    fn = false_negative(y_true, y_pred)
+    return tp / (tp + fn)
+
+def f1_score(y_true, y_pred):
+    precision = precision(y_true, y_pred)
+    recall = precision (y_true, y_pred)
+    return 2 * precision * recall / (precision + recall)
+
 
